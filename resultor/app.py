9acf31d5b7779f9583a16fbfa71e2bda21a4c456
daniel anggrianto <daniela@aweber.com>
2014-04-14 13:50:15 -0400

    basic prototype

--- resultor/app.py
+++ resultor/app.py
@@ -1,13 +1,18 @@
 from __future__ import unicode_literals
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def Index():
-    return 'Hello World'
+    return render_template('index.html')
+
+
+@app.route("/pass")
+def pass_test():
+    return render_template('base_test.html')
 
 if __name__ == '__main__':
     app.run()
