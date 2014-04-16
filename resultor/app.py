38e020b7a1413f5641e74e10614d983e6c9268d7
daniel anggrianto <daniela@aweber.com>
2014-04-16 13:12:51 -0400

    app: add put backend

--- resultor/app.py
+++ resultor/app.py
@@ -1,11 +1,16 @@
 from __future__ import unicode_literals
 
+import time
+
 from flask import Flask, render_template, request
 from flask.ext.pymongo import PyMongo
+from flask.ext.restful import Api, Resource
+
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'resultor'
 mongo = PyMongo(app)
+api = Api(app)
 
 
 @app.route("/")
@@ -49,6 +54,43 @@ def show_test(module, name):
     average = mongo.db.results.aggregate(aggregation)['result'][0]['average']
     return render_template(
         'base_test.html', info=info, results=results, avg=round(average, 2))
+
+
+####
+##
+## REST
+##
+####
+
+class Result(Resource):
+    def put(self):
+        json = request.get_json(force=True)
+        json['timestamp'] = int(round(time.time() * 1000))
+        mongo.db.results.insert(json)
+        module = json['module']
+        name = json['name']
+        aggregation = [
+            {'$match': {
+                'module': module, 'name': name, 'status': 'pass'}},
+            {'$group': {
+                '_id': {'name': '$name', 'module': '$module'},
+                'average': {'$avg': "$duration"}}}]
+        average = mongo.db.results.aggregate(
+            aggregation)['result'][0]['average']
+        count = mongo.db.results.find(
+            {'name': name, 'module': module}).count()
+        fail_count = mongo.db.results.find(
+            {'name': name, 'module': module, 'status': 'fail'}).count()
+        mongo.db.info.update(
+            {'name': name, 'module': module},
+            {'$set': {
+                'average': average, 'count': count, 'fail_count': fail_count
+            }}, True)
+        return {}
+
+
+api.add_resource(Result, '/api/result')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
